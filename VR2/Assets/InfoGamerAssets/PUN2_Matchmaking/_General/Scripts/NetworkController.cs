f4f2f644645beae72b40cf6585cc86f5798b95d2
Kenneth_KEi <hotdinner@gmail.com>
2020-10-19 21:32:18 +1100

    1

--- VR2/Assets/InfoGamerAssets/PUN2_Matchmaking/_General/Scripts/NetworkController.cs
+++ VR2/Assets/InfoGamerAssets/PUN2_Matchmaking/_General/Scripts/NetworkController.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
-    /******************************************************
-    * Refer to the Photon documentation and scripting API for official definitions and descriptions
-    * 
-    * Documentation: https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
-    * Scripting API: https://doc-api.photonengine.com/en/pun/v2/index.html
-    * 
-    * If your Unity editor and standalone builds do not connect with each other but the multiple standalones
-    * do then try manually setting the FixedRegion in the PhotonServerSettings during the development of your project.
-    * https://doc.photonengine.com/en-us/realtime/current/connection-and-authentication/regions
-    *
-    * ******************************************************/
+
     [SerializeField]
     bool connectOnStart;
 
