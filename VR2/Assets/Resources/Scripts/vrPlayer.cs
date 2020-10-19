e66bb10fd1e6f50f0577371f6d1c22d63f9c6ee0
Kenneth_KEi <hotdinner@gmail.com>
2020-10-20 10:34:37 +1100

    Nothing changed 2

--- VR2/Assets/Resources/Scripts/vrPlayer.cs
+++ VR2/Assets/Resources/Scripts/vrPlayer.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class vrPlayer : MonoBehaviour
 {
+    private PhotonView PV;
     public Camera _camera;
+    public AudioListener _AL;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PV = GetComponent<PhotonView>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!PV.IsMine)
+        {
+            Destroy(_camera);
+            Destroy(_AL);
+            
+        }
     }
 }
