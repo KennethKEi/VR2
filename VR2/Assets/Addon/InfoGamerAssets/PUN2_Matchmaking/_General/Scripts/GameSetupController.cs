0f80198367c43681256bfa66a7d828e3f5c4c1f6
Kenneth_KEi <hotdinner@gmail.com>
2020-10-20 14:41:51 +1100

    added boy

---
+++ VR2/Assets/Addon/InfoGamerAssets/PUN2_Matchmaking/_General/Scripts/GameSetupController.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using System.IO;
+using UnityEngine;
+
+public class GameSetupController : MonoBehaviour
+{
+    // This script will be added to any multiplayer scene
+    void Start()
+    {
+        CreatePlayer(); //Create a networked player object for each player that loads into the multiplayer scenes.
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), new Vector3(11.13f, 0.96f, 4.998f), Quaternion.identity);
+    }
+}
